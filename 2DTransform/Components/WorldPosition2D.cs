8303b104b9934eedc8e3158d6ad541ea9e2f7c1e
Kyle <kyle.studd@gmail.com>
2023-04-11 14:10:00 -0400

    Made WorldPosition2D a Ghosted

--- 2DTransform/Components/WorldPosition2D.cs
+++ 2DTransform/Components/WorldPosition2D.cs
@@ -4,8 +4,9 @@ using Unity.NetCode;
 
 namespace NSprites
 {
+    [GhostComponent()]
     public struct WorldPosition2D : IComponentData
     {
-        public float2 value;
+        [GhostField] public float2 value;
     }
 }
